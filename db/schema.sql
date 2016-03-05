1f4925ab7e1327a464163fd5e640488565af0f72
Nick Pierce <nick.pierce@jamfsoftware.com>
2016-03-05 15:59:55 -0600

    Included file for databse schema and file for installing and setting up your mysql database

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE AED;
+
+USE AED;
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `latitude` float(8,6) DEFAULT NULL,
+  `longitude` float(9,6) DEFAULT NULL,
+  `CreatedDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedDate` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ CREATE TABLE `aed_description` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `FilePath` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `Reference_URL` varchar(256) DEFAULT NULL,
+  `CreatedDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedDate` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `ExpirationDate` datetime DEFAULT NULL,
+  `LocationId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `LocationId` (`LocationId`),
+  CONSTRAINT `aed_description_ibfk_1` FOREIGN KEY (`LocationId`) REFERENCES `Location` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
---
+++ db/setup.md
@@ -0,0 +1,13 @@
+Download mysql version 5.7. 
+
+Homebrew is an easy install. 
+Download home-brew:
+	http://brew.sh
+
+Download mysql using home-brew
+	brew install mysql
+
+Login to myql:
+	mysql -u root -p
+	
+On password prompt, hit enter for. There is no specified password.
