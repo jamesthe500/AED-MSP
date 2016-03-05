da152aeea0e88945efc1185fea8775e433720540
jamesthe500 <jamesddaniels@gmail.com>
2016-03-05 16:39:51 -0600

    Serves public folder

--- server/main.js
+++ server/main.js
@@ -1,9 +1,12 @@
 
 var express = require('express')
 var app = express()
+var path = require('path')
 
 var route = require('./route')
 route(app)
+
+app.use("/", express.static(path.resolve(__dirname + "/public")))
 
 app.listen(7799, function (err) {
 	if (err) console.error(err)
