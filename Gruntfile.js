c8a0c0582a702f910729b602c728f6f5b23e939a
jamesthe500 <jamesddaniels@gmail.com>
2016-03-05 16:00:08 -0600

    Adds Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            }/*,
+             build: {
+             src: 'client/scripts/app.js',
+             dest: 'server/public/assets/scripts/app.min.js'
+             }*/ // commented out so line numbers show up
+        },
+        copy: {
+            css: {
+                expand: true,
+                cwd: 'client',
+                src: [
+                    "styles/*"
+                ],
+                "dest": "server/public/assets/"
+            },
+            images: {
+                expand: true,
+                cwd: 'client',
+                src: [
+                    "images/**"
+                ],
+                "dest": "server/public/assets/"
+
+            },
+            html: {
+                expand: true,
+                cwd: 'client',
+                src: [
+                    "views/*"
+                ],
+                "dest": "server/public/assets/"
+
+            },
+            scripts: {
+                expand: true,
+                cwd: 'client',
+                src: [
+                    "scripts/*"
+                ],
+                "dest": "server/public/assets/"
+            },
+            angular: {
+                expand: true,
+                cwd: "node_modules/",
+                src: [
+                    "angular/angular.min.js",
+                    "angular/angular.min.js.map",
+                    "angular/angular-csp.css"
+                ],
+                "dest": "server/public/vendors/"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['copy', 'uglify']);
+};
