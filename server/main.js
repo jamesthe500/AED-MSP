4bab7e9ae47165290fc06c1f9fa5b079284df34a
Nick Pierce <nick.pierce@jamfsoftware.com>
2016-03-05 22:57:59 -0600

    Define engines, and allow port to be set by heroku

--- server/main.js
+++ server/main.js
@@ -2,14 +2,15 @@
 var express = require('express')
 var app = express()
 var path = require('path')
+var port = process.env.PORT || 7799
 
 var route = require('./route')
 route(app)
 
 app.use('/', express.static(path.resolve(__dirname + '/public')))
 
-app.listen(7799, function (err) {
+app.listen(port, function (err) {
 	if (err) console.error(err)
 })
 
-console.log('Running on http://localhost:7799...')
+console.log('Running on http://localhost:' + port + '...')
