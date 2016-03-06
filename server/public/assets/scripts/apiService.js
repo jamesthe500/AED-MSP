3eb6d10f2864321e3721ab014c7f0599c9f151f9
Sean Hickey <shickey@blakeschool.org>
2016-03-06 14:03:20 -0600

    Messing with API enpoints and files

--- server/public/assets/scripts/apiService.js
+++ server/public/assets/scripts/apiService.js
@@ -7,23 +7,25 @@
   app.factory("aedApi", ['$cacheFactory', '$http' function($cacheFactory, '$http') {
     
     var api = {};
-    var aedCache = $cacheFactory('aed');
-    
-    api.getAed = function(aedId) {
-      if (aedCache.get(aedId)) { return aedCache.get(aedId); };
-      
-      return $http.get('AED ID URL').then(function(response) {
-        cache.put(response.id, response.data);
-        return response;
-      });
-    };
     
     api.queryAeds = function(lat, lon) {
-      return $http.get('AED LOCATION QUERY URL').then(function(response) {
+      return $http.get('AED QUERY ENDPOINT').then(function(response) {
         return response.data
       });
     };
     
+    api.createAed = function(aed) {
+      var formData = new FormData();
+      
+      for (key in aed) {
+        formData.append(key, aed[key])
+      }
+      
+      return $http.post('/locations', formData, {
+        transformRequest: angular.identity,
+        headers: { 'Content-Type' : undefined }
+      });
+    }
     
     return api;
     
