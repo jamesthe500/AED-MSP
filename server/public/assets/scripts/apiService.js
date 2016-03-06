aaf6e7ede3bd3f0e38fcf300b4e07c36236e7404
sdpeyton <stephendpeyton@gmail.com>
2016-03-06 14:37:01 -0600

    changes

--- server/public/assets/scripts/apiService.js
+++ server/public/assets/scripts/apiService.js
@@ -2,25 +2,22 @@
   
   var API_BASE_URL = '';
   
-  var app = angular.module("AedLocator");
+  var app = angular.module("aedLocatorApp");
   
-  app.factory("aedApi", ['$cacheFactory', '$http' function($cacheFactory, '$http') {
+  app.factory("aedApi", ['$cacheFactory', '$http', function($cacheFactory, $http) {
     
     var api = {};
     
     api.queryAeds = function(lat, lon) {
-      return $http.get('AED QUERY ENDPOINT').then(function(response) {
+      return $http.get('/locations?lat=' + lat + "&long=" + lon).then(function(response) {
         return response.data
+      }, function(error) {
+        console.log("error while querying api");
+        console.log(error);
       });
     };
     
-    api.createAed = function(aed) {
-      var formData = new FormData();
-      
-      for (key in aed) {
-        formData.append(key, aed[key])
-      }
-      
+    api.createAed = function(aedFormData) {
       return $http.post('/locations', formData, {
         transformRequest: angular.identity,
         headers: { 'Content-Type' : undefined }
