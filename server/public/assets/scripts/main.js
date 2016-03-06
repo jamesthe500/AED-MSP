f17c5334c672b3031de3ff15a37e90ca17d48ff3
jamesthe500 <jamesddaniels@gmail.com>
2016-03-06 11:12:21 -0600

    Sets up hide/show with adding views

--- server/public/assets/scripts/main.js
+++ server/public/assets/scripts/main.js
@@ -2,7 +2,7 @@ var aedLocatorApp = angular.module('aedLocatorApp', ['ngRoute']);
 
 
 aedLocatorApp.config(['$routeProvider', function($routeProvider){
-  
+
   $routeProvider.
     when('/', {
       templateUrl: '/assets/views/home-template.html'
@@ -19,6 +19,57 @@ aedLocatorApp.config(['$routeProvider', function($routeProvider){
 
 aedLocatorApp.controller('AedController', ['$scope', function($scope) {
 
+    $scope.addView0 = true;
+    $scope.addView1 = false;
+    $scope.addView2 = false;
+    $scope.addView3 = false;
+    $scope.addView4 = false;
+    $scope.addView5 = false;
+
+    $scope.correctAedLocation = function(preExistingAed) {
+        $scope.addView0 = !$scope.addView0;
+        // TODO run a query here to find the closest AED again, in case the map has moved. Then it'll be View1 OR View4
+        if(preExistingAed){
+            $scope.addView1 = !$scope.addView1;
+        } else {
+            $scope.addView4 = !$scope.addView4;
+        }
+    };
+    $scope.thatWasTheSameAed = function() {
+        $scope.addView1 = !$scope.addView1;
+        $scope.addView2 = !$scope.addView2;
+    };
+    $scope.thatWasNotTheSameAed = function() {
+        // TODO bring up the next closest AED, until we run out of AEDs within 20m
+        $scope.addView1 = !$scope.addView1;
+        $scope.addView4 = !$scope.addView4;
+    };
+    $scope.updateAedDescription = function() {
+        $scope.addView2 = !$scope.addView2;
+        $scope.addView4 = !$scope.addView4;
+    };
+    $scope.confirmNearestAed = function() {
+        $scope.addView2 = !$scope.addView2;
+        $scope.addView5 = !$scope.addView5;
+    };
+    $scope.updateAedPhoto = function() {
+        $scope.addView2 = !$scope.addView2;
+        $scope.addView3 = !$scope.addView3;
+    };
+    $scope.setAedDescription = function() {
+        $scope.addView4 = !$scope.addView4;
+        $scope.addView3 = !$scope.addView3;
+    };
+    $scope.photoTaken = function() {
+        $scope.addView3 = !$scope.addView3;
+        $scope.addView5 = !$scope.addView5;
+    };
+    $scope.devStartOver = function() {
+        $scope.addView5 = !$scope.addView5;
+        $scope.addView0 = !$scope.addView0;
+    };
 
 
+
+  
 }]);
